69ff0c3b29fe64164b8bf5c24e1da816ad669d29
ofb-pdl <136820984+ofb-pdl@users.noreply.github.com>
2023-06-20 13:41:02 +0200

    Adaptation du paramétrage à la région

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
-conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Pays de la Loire"
+conf_titre <- "Suivi des étiages (ONDE) en Pays de la Loire"
 conf_auteur <- "OFB - Direction régionale Pays de la Loire"
 conf_dep <- c("44", "49", "53", "72", "85")
